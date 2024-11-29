d641aa7267134f3774d98b7189d32c9318909338
thanh <thanhduc17k@gmail.com>
2024-11-29 10:58:06 +0700

    Merge branch 'dev' of https://gitlab.com/hoanglong2002.jvb/FE-AutoCareerBridge

--- src/Template/BusinessTemplate/BusinessTemplate.jsx
+++ src/Template/BusinessTemplate/BusinessTemplate.jsx
@@ -1,5 +1,69 @@
+import {Header} from "../../Component/HeaderComponent/Header";
+import {SideBar} from "../../Component/SideBarComponent/SideBar";
+import {Outlet} from "react-router-dom";
+import {useState} from "react";
+import {useSelector} from "react-redux";
+
 export function BusinessTemplate() {
-    return (
-        <></>
-    )
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { userData } = useSelector((state) => state.UserReducer);
+  const toggleSidebar = () => {
+    setIsMenuOpen(!isMenuOpen); // Đảo ngược trạng thái sidebar khi nhấn vào nút
+  };
+  const menuItems = [
+    {
+      label: "Doanh nghiệp",
+      icon: "fa-solid fa-school",
+      subMenu: [
+        {
+          label: "Danh sách ngành nghề",
+          link: "/industry-manager",
+          icon: "",
+        },
+        { label: "Danh sách khoa", link: "/section-manager", icon: "" },
+        { label: "Danh sách ngành ", link: "/major-manager", icon: "" },
+      ],
+    },
+    {
+      label: "Sự kiện",
+      icon: "fa-solid fa-store",
+      subMenu: [
+        { label: "Danh sách sự kiện", link: "/workshop-manager", icon: "" },
+        { label: "Công ty tham gia", link: "/section-manager", icon: "" },
+        { label: "Công ty chờ duyệt", link: "/major-manager", icon: "" },
+      ],
+    },
+    {
+      label: "Công việc",
+      icon: "fa-solid fa-briefcase",
+      subMenu: [
+        { label: "Danh sách công việc", link: "/workshop-manager", icon: "" },
+        { label: "Đề xuất công việc", link: "/section-manager", icon: "" },
+        { label: "Yêu cầu hợp tác", link: "/section-manager", icon: "" },
+      ],
+    },
+    {
+      label: "Thống kê",
+      icon: "home",
+      subMenu: [{ label: "123", link: "", icon: "" }],
+    },
+  ];
+  return (
+    <>
+      <div
+        id="root"
+        className={`root tm--primary-mn ${isMenuOpen ? "mn--max" : "mn--min"}`}
+      >
+        <Header toggleSidebar={toggleSidebar} link={"/university"} />
+        <SideBar
+          userName={userData.username}
+          userRole={userData.role.name}
+          profileImg="./assets/img/profile-photos/1.png"
+          caption="Quản lý trường đại học"
+          menuItems={menuItems}
+        />
+        <Outlet />
+      </div>
+    </>
+  );
 }
