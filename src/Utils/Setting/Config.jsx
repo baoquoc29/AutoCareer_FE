021eb4a9b02ac564ca31bba10ab542b185cab0f2
TranMinhHieu <tranhieu02042002@gmail.com>
2024-11-29 10:58:46 +0700

    sua port

--- src/Utils/Setting/Config.jsx
+++ src/Utils/Setting/Config.jsx
@@ -1,18 +1,17 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const DISPLAY_LOADING = "DISPLAY_LOADING";
+export const HIDE_LOADING = "HIDE_LOADING";
 
-export const DISPLAY_LOADING = 'DISPLAY_LOADING';
-export const HIDE_LOADING = 'HIDE_LOADING';
+export const DOMAIN = "http://localhost:1111/auto-career";
+export const TOKEN = "accessToken";
 
-export const DOMAIN = 'http://localhost:1111/auto_career'
-export const TOKEN = 'accessToken'
-
-export const USER_LOGIN = 'USER_LOGIN';
+export const USER_LOGIN = "USER_LOGIN";
 
 export const STATUS_CODE = {
-    SUCCESS: 200,
-    NOT_FOUND: 404,
-    SERVER_ERROR: 500,
-    BAD_REQUEST:400
-}
+  SUCCESS: 200,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+  BAD_REQUEST: 400,
+};
