37c737bbbfd6327afac771833df05959b54514d2
TranMinhHieu <tranhieu02042002@gmail.com>
2024-12-02 17:19:31 +0700

    finish get eployee all, get by id

---
+++ src/Page/Business/EmployeeManager/EmployeeManager.jsx
@@ -0,0 +1,112 @@
+import React, {useEffect, useState} from "react";
+import {Button, Card, Input} from "antd";
+import "antd/dist/reset.css";
+import {PlusOutlined} from "@ant-design/icons";
+import {useDispatch, useSelector} from "react-redux";
+import {
+    create_employee,
+    delete_employee_id,
+    get_all_employees,
+    get_employee_by_id
+} from "../../../Redux/actions/EmployeeThunk";
+import EmployeeTable from "./EmployeeTable";
+import EmployeeDetail from "./EmployeeDetail";
+import {toast} from "react-toastify";
+
+const EmployeeManager = () => {
+    const dispatch = useDispatch();
+    const employees = useSelector(state => state.EmployeeReducer.employees);
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
+    // const [searchText, setSearchText] = useState("");
+    // const [filteredData, setFilteredData] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        dispatch(get_all_employees());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (selectedEmployee) {
+            dispatch(get_employee_by_id(selectedEmployee.id));
+        }
+    }, [dispatch]);
+
+    //xem chi tiet nhan vien
+    const handleInfo = (record) => {
+        setSelectedEmployee(record);
+        setOpen(true);
+    };
+
+    //tao moi nhan vien
+    const handleSubmit = (values) => {
+        dispatch(create_employee(values))
+            .then(() => {
+                dispatch(get_all_employees());
+
+            })
+    };
+
+    //xoa nhan vien
+    const handleDelete = (employeeId) => {
+        dispatch(delete_employee_id(employeeId))
+            .then(() => {
+                toast.success("Xóa nhân viên thành công")
+                dispatch(get_all_employees())
+            })
+            .catch((error) => {
+                toast.success(error.messages)
+            })
+    }
+
+    const data = employees.map((employee, index) => ({
+        id: employee.id,
+        stt: index + 1,
+        employeeCode: employee.employeeCode,
+        name: employee.name,
+        email: employee.email,
+        status: employee.status,
+        gender: employee.gender,
+        phone: employee.phone,
+        dateOfBirth: employee.dateOfBirth,
+        address: employee.address,
+    }));
+
+    return (
+        <div>
+            <section id="content" className="content">
+                <div className="content__header content__boxed rounded-0">
+                    <div className="content__wrap">
+                        <section>
+                            <div className="container mt-5">
+                                <div className="row">
+                                    <div className="col-12 mb-3">
+                                        <Card title="Danh sách nhân viên">
+                                            <div className="table-responsive">
+                                                <div className="d-flex mb-3">
+                                                    <Button type="primary" icon={<PlusOutlined/>}>
+                                                        Thêm mới
+                                                    </Button>
+                                                </div>
+                                                <EmployeeTable
+                                                    data={data}
+                                                    onInfo={handleInfo}
+                                                    onDetle={handleDelete}
+                                                />
+                                            </div>
+                                        </Card>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </section>
+            <EmployeeDetail
+                open={open}
+                onClose={() => setOpen(false)}
+                employee={selectedEmployee}
+            />
+        </div>
+    );
+}
+export default EmployeeManager;
