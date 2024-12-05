214dc1063a7a03bd3b5b0bf7c7df8a703ab2a90d
Cẩm Hoàng <hoangcam.jvb@gmail.com>
2024-12-05 02:09:53 +0000

    CRUD Sub admin

--- src/Template/AdminTemplate/AdminTemplate.jsx
+++ src/Template/AdminTemplate/AdminTemplate.jsx
@@ -1,5 +1,68 @@
+import {Header} from "../../Component/HeaderComponent/Header";
+import {SideBar} from "../../Component/SideBarComponent/SideBar";
+import {Outlet} from "react-router-dom";
+import {useState} from "react";
+import {useSelector} from "react-redux";
+import {DOMAIN} from "../../Utils/Setting/Config";
+
 export function AdminTemplate() {
+    const subAdmin = useSelector(state => state.UserReducer.userData?.subAdmin);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const {userData} = useSelector((state) => state.UserReducer);
+    const toggleSidebar = () => {
+        setIsMenuOpen(!isMenuOpen);  // Đảo ngược trạng thái sidebar khi nhấn vào nút
+    };
+    const menuItems = [
+        // {
+        //     label: "Trang chủ", link: '/admin',
+        // },
+        {
+            label: 'Quản lý tài khoản',
+            icon: 'fa-solid fa-school',
+            subMenu: [
+                {label: 'Tài khoản quản trị viên', link: '/sub-admin-manager', icon: ''},
+                {label: 'Tài khoản doanh nghiệp', link: '/section-manager', icon: ''},
+                {label: 'Tài khoản trường học', link: '/major-manager', icon: ''},
+            ]
+        },
+
+        {
+            label: 'Quản lý tin tuyển dụng',
+            icon: 'fa-solid fa-store',
+            subMenu: [
+                {label: 'Yêu cầu chờ duyệt', link: '/section-manager', icon: ''},
+                {label: 'Danh sách tuyển dụng', link: '/workshop-manager', icon: ''},
+            ]
+        },
+        {
+            label: 'Quản lý hội thảo',
+            icon: 'fa-solid fa-briefcase',
+            subMenu: [
+                {label: 'Danh sách hội thảo', link: '/workshop-manager', icon: ''},
+                {label: 'Yêu cầu chờ duyệt', link: '/section-manager', icon: ''},
+            ]
+        },
+        {
+            label: 'Thống kê',
+            icon: 'home',
+            subMenu: [
+                {label: '123', link: '', icon: ''},
+            ]
+        }
+    ];
     return (
-        <></>
+        <>
+            <div id="root" className={`root tm--primary-mn ${isMenuOpen ? 'mn--max' : 'mn--min'}`}>
+                <Header toggleSidebar={toggleSidebar} link={'/admin'}/>
+                <SideBar
+                    userName={userData.username}
+                    userRole={userData.role.name}
+                    // profileImg={`${DOMAIN}/api/v1/image/resource?imageId=${subAdmin.subAdminImageId}`}
+                    caption="Quản lý hệ thống"
+                    menuItems={menuItems}
+                />
+                <Outlet/>
+            </div>
+        </>
     )
 }
--- src/Utils/Setting/Config.jsx
+++ src/Utils/Setting/Config.jsx
@@ -6,6 +6,7 @@ export const DISPLAY_LOADING = 'DISPLAY_LOADING';
 export const HIDE_LOADING = 'HIDE_LOADING';
 
 export const DOMAIN = 'http://localhost:1111/auto-career'
+export const GET_IMAGE_URI = `${DOMAIN}/api/v1/image/resource?imageId=`
 export const TOKEN = 'accessToken'
 
 export const USER_LOGIN = 'USER_LOGIN';
