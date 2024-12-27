4700547f08bae7b0b162eeba7a0cca12809ebe70
baowork <99949277+baoquoc29@users.noreply.github.com>
2024-12-27 09:14:04 +0700

    Sửa logic xoá workshop

--- src/Component/HeaderComponent/HeaderPortal/HeaderPortal.jsx
+++ src/Component/HeaderComponent/HeaderPortal/HeaderPortal.jsx
@@ -82,6 +82,7 @@ const HeaderPortal = () => {
             window.open('/job-manager', '_blank');
         }
         else if (userData?.role?.name === "SUB-ADMIN") {
+            window.open('/admin', '_blank');
         } else if (userData?.role?.name === "EMPLOYEE") {
             window.open('/job-manager', '_blank');
         }
