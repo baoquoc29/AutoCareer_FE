13d1784627ddb31e9f8f8aa0bde8f8f20bec4216
TranMinhHieu <tranhieu02042002@gmail.com>
2024-12-29 02:24:12 +0700

    pull code

--- src/Utils/Setting/Config.jsx
+++ src/Utils/Setting/Config.jsx
@@ -16,16 +16,13 @@ export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
 export const DISPLAY_LOADING = 'DISPLAY_LOADING';
 export const HIDE_LOADING = 'HIDE_LOADING';
 export const CLEAN_LOCAL_STORAGE = 'CLEAN_LOCAL_STORAGE';
+
 // export const DOMAIN = 'http://192.168.0.152:8081/auto-career'
 export const DOMAIN = 'http://localhost:8081/auto-career'
 //domain firebase
-// export const DOMAIN = 'https://192.168.1.106:8081/auto-career'
 
 export const GET_IMAGE_URI = `${DOMAIN}/api/v1/image/resource?imageId=`
 // export const GET_IMAGE_URI = `http://192.168.0.152:8081/auto-career/api/v1/image/resource?imageId=`
-
-
-
 
 export const TOKEN = 'accessToken'
 
